65a32160f4581774007d7ed13853ded9be46fd9c
Neeraj Komuravalli <neeraj.komuravalli@labyrinth.ai>
2021-04-14 19:05:21 +0530

    Adding filuure method to post method test

--- post_test.go
+++ post_test.go
@@ -8,8 +8,8 @@ import (
 	"testing"
 )
 
-func TestPostData(t *testing.T) {
-	var jsonStr = []byte(`{"id":"111","value":111}`)
+func postSampleData(t *testing.T, data string) *httptest.ResponseRecorder {
+	var jsonStr = []byte(data)
 
 	req, err := http.NewRequest("GET", "/Post/", bytes.NewBuffer(jsonStr))
 	if err != nil {
@@ -23,8 +23,29 @@ func TestPostData(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	return rr
+}
+
+func TestPostDataSuccess(t *testing.T) {
+	jsonStr := `{"id":"111","value":111}`
+	rr := postSampleData(t, jsonStr)
 	expected := `{"success":true,"data":{"id":"111","value":111}}`
 	// the strings that you read from STDIN has a trailing \n that's why we need to trip it
+	if strings.TrimRight(rr.Body.String(), "\n") != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestPostDataFailure(t *testing.T) {
+	jsonStr := `{"id":"111","value":111}`
+	postSampleData(t, jsonStr)
+	rr := postSampleData(t, jsonStr)
+	expected := `{"success":false,"error":"The id 111 you want to add is already present in the table, please use Update method to update any value"}`
 	if strings.TrimRight(rr.Body.String(), "\n") != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)

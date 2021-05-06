efdb216e93bddafef5d0c9ae2916595bb6c948da
Neeraj Komuravalli <komunee2710@gmail.com>
2021-05-06 20:22:57 +0530

    Adding client example for TotalNumberOfActiveNodes

--- chordNetworkAdmin/sampleClient/main.go
+++ chordNetworkAdmin/sampleClient/main.go
@@ -21,21 +21,26 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Before adding any node : %v\n", activeNode)
-	node := chordAdminModel.Node{Id: "22"}
-	success, err := adminClient.AddActiveNode(context.Background(), &node)
+	totalNumberOfNodes, err := adminClient.TotalNumberOfActiveNodes(context.Background(), new(chordAdminModel.NumberOfActiveNodes))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Adding of node was success : %v\n", success.Success)
-	node = chordAdminModel.Node{Id: "102"}
-	success, err = adminClient.AddActiveNode(context.Background(), &node)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Adding of node was success : %v\n", success.Success)
-	activeNodeLater, err := adminClient.GetActiveNodes(context.Background(), new(chordAdminModel.ActiveNodesRequest))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("After adding node : %v\n", activeNodeLater)
+	fmt.Printf("Total number of nodes : %v\n", totalNumberOfNodes.Number)
+	// node := chordAdminModel.Node{Id: "22"}
+	// success, err := adminClient.AddActiveNode(context.Background(), &node)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Adding of node was success : %v\n", success.Success)
+	// node = chordAdminModel.Node{Id: "102"}
+	// success, err = adminClient.AddActiveNode(context.Background(), &node)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Adding of node was success : %v\n", success.Success)
+	// activeNodeLater, err := adminClient.GetActiveNodes(context.Background(), new(chordAdminModel.ActiveNodesRequest))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("After adding node : %v\n", activeNodeLater)
 }

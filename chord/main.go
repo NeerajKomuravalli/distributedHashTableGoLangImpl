d080870c68645c3670c53d5b8429407ac0d5dcd0
Neeraj Komuravalli <komunee2710@gmail.com>
2021-05-06 17:26:51 +0530

    Removing all the code used to test the chordAdminNetwork

--- chord/main.go
+++ chord/main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
-	"github.com/NeerajKomuravalli/distributedHashTableGoLangImpl/chord"
-	"github.com/NeerajKomuravalli/distributedHashTableGoLangImpl/chordNetworkAdmin"
-	"github.com/NeerajKomuravalli/distributedHashTableGoLangImpl/models/proto/chordAdmin/chordAdminModel"
+	"github.com/NeerajKomuravalli/distributedHashTableGoLangImpl/chord/chord"
 	"github.com/NeerajKomuravalli/distributedHashTableGoLangImpl/models/proto/chordNode/chordNodeModel"
-	"google.golang.org/grpc"
 )
 
 func main() {
@@ -27,35 +23,4 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(resp.Message)
-
-	grpcServer := chordNetworkAdmin.ServeChordNetworkAdmin(":9090")
-	defer grpcServer.Stop()
-	clientConn, err := grpc.Dial(":9090", grpc.WithInsecure())
-	if err != nil {
-		log.Fatal("Unable to get a connection")
-	}
-	defer clientConn.Close()
-	adminClient := chordAdminModel.NewChordAdminClient(clientConn)
-	activeNode, err := adminClient.GetActiveNodes(context.Background(), new(chordAdminModel.ActiveNodesRequest))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Before adding any node : %v\n", activeNode)
-	node := chordAdminModel.Node{Id: "22"}
-	success, err := adminClient.AddActiveNode(context.Background(), &node)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Adding of node was success : %v\n", success.Success)
-	node = chordAdminModel.Node{Id: "102"}
-	success, err = adminClient.AddActiveNode(context.Background(), &node)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Adding of node was success : %v\n", success.Success)
-	activeNodeLater, err := adminClient.GetActiveNodes(context.Background(), new(chordAdminModel.ActiveNodesRequest))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("After adding node : %v\n", activeNodeLater)
 }
